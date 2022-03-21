bc4464e0d6dd791efd375804c4896fb78a8ec81b
KitWilliams07 <95001233+KitWilliams07@users.noreply.github.com>
2022-03-21 11:33:09 -0400

    Add files via upload

---
+++ Multiple_Linear_Regression/MechaCarChallenge.R
@@ -0,0 +1,9 @@
+# Libraries
+library(dplyr)
+
+# Deliverable 1
+mecha_car <- read.csv('MechaCar_mpg.csv')
+
+multiple_regression_model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_car)
+summary(multiple_regression_model)
+
